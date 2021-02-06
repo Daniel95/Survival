661799cf043dc44a49f280d148805e5c3ea2fcc4
Daniel Brand <Daniel95@users.noreply.github.com>
2021-02-06 23:25:26 +0100

    Add player controller

---
+++ TerraceJam1Survival/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float runSpeed = 40f;
+
+    private CharacterController2D controller;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        //if (Input.GetButtonDown("Crouch"))
+        //{
+        //    crouch = true;
+        //}
+        //else if (Input.GetButtonUp("Crouch"))
+        //{
+        //    crouch = false;
+        //}
+
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+}
